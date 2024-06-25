97bb613c688e645c414816825b224f1343336967
Jaden Lee <changyeob99@gmail.com>
2024-06-26 00:13:11 +0900

    conditionals exercise1

---
+++ day_3/conditionals_exercise1.py
@@ -0,0 +1,10 @@
+item = input('What item does life give you? ')
+
+if item == 'lemon' or item == 'lemons':
+    print('Make lemonade.')
+elif item == 'orange' or item == 'oranges':
+    print('Make orange juice.')
+elif item == 'money':
+    print('Buy oranges or lemons to make lemonade or orange juice.')
+else:
+    print('Love is not an item!')
