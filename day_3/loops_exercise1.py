0c23ea5e7919fe7fe904bdbf962ab03c06f30615
Jaden Lee <changyeob99@gmail.com>
2024-06-26 01:06:40 +0900

    loops exercise1

---
+++ day_3/loops_exercise1.py
@@ -0,0 +1,12 @@
+mydict = {
+    'avocado': '1.99$',
+    'bell pepper': '0.89$',
+    'strawberries': '2.77$',
+    'lemon': '0.89$',
+    'blueberries': '7.99$',
+    'watermelon': '5.99$',
+    'peach': '1.85$'
+}
+
+for key in mydict:
+    print(key)
