83b485255d31ac7a18e2737a1bcd82ab83de380c
Jaden Lee <changyeob99@gmail.com>
2024-06-26 01:13:56 +0900

    loops exercise3

---
+++ day_3/loops_exercise2.py
@@ -0,0 +1,15 @@
+mydict = {'avocado':'1.99$',
+          'bell pepper':'0.89$',
+          'strawberries':'2.77$',
+          'lemon':'0.89$',
+          'blueberries':'7.99$',
+          'watermelon':'5.99$',
+          'peach':'1.85$'}
+
+keys = list(mydict.keys())
+
+index = 0
+
+while index < len(keys):
+    print(keys[index])
+    index += 1
