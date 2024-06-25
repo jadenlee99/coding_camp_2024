17f56de66ea93ea462259d764bb343b5c8b32e75
Jaden Lee <changyeob99@gmail.com>
2024-06-26 05:51:50 +0900

    loops exercise10

---
+++ day_3/loops_exercise10.py
@@ -0,0 +1,11 @@
+text = r"""
+Because I am sad, it is very difficult to open the doorknob and leave my house.
+Some people tell me taht the solution is to just concentrate and I will stop
+being sad, but they are clearly spouting nonsense. At least when my close
+friends found that I am sad, they just accepted it and have been there for me
+to help me get out of the house. Maybe one day I will be able to stop being
+sad and be more open about it.
+"""
+
+replaced_text = text.replace('sad', 'secretly a duck')
+print(replaced_text)
