cf207c09673697fdca6d670cf22bd888612688de
jadenlee99 <changyeob99@gmail.com>
2024-07-10 13:01:31 -0600

    240710

---
+++ day_5/mylibrary/mylibrary.py
@@ -0,0 +1,15 @@
+def dog():
+    print('Woof!')
+
+def cat():
+    print('Miau!')
+
+def human():
+    print('Wassup bro!')
+
+def cow():
+    print('Moo!')
+
+
+if __name__ == '__main__':
+    print('Hello World')
